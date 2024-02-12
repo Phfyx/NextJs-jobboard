e425751c88a950062116bfa8b981e0787fbb4c15
Phfyx <97990186+Phfyx@users.noreply.github.com>
2024-02-12 21:16:34 +0100

    FEAT: added Job list

---
+++ src/components/JobFilterSidebar.tsx
@@ -0,0 +1,5 @@
+export default function JobFilterSidebar () {
+    return <aside> 
+        sidebar
+    </aside>
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import {formatDistanceToNowStrict} from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount)
+}
+
+export function relativeDate(from: Date) {
+  return formatDistanceToNowStrict(from, { addSuffix: true })
 }
