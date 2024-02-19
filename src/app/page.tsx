98cb7b5dd684104d749c96b5e16e14d9f606698d
Phfyx <97990186+Phfyx@users.noreply.github.com>
2024-02-19 17:25:43 +0100

    Add Footer component and update title in layout.tsx and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,58 @@
-import JobListItem from "@/components/JobListItem";
 import JobFilterSidebar from "@/components/JobFilterSidebar";
-import prisma from "@/lib/prisma";
 import H1 from "@/components/ui/h1";
 import JobResults from "@/components/JobResults";
 import { JobFiltervalues } from "@/lib/validation";
+import { Metadata } from "next";
 
 interface PageProps {
-  searchParams:{
+  searchParams: {
     query?: string;
     type?: string;
     location?: string;
     remote?: string;
-  }
+  };
 }
 
-export default async function Home({searchParams: {query, type, location, remote}}: PageProps) {
+function getTitle({ query, type, location, remote }: JobFiltervalues) {
+  const titlePrefix = query
+    ? `${query} jobs`
+    : type
+      ? `${type} developper jobs`
+      : remote
+        ? "Remote developper jobs"
+        : "All Developper jobs";
+
+  const titleSuffix = location ? ` in ${location}` : "";
+
+  return `${titlePrefix}${titleSuffix}`;
+}
+
+export function generateMetadata({
+  searchParams: { query, type, location, remote },
+}: PageProps): Metadata {
+  return {
+    title: `${getTitle({ query, type, location, remote: remote === "true" })} | Flow Jobs`,
+  };
+}
+
+export default async function Home({
+  searchParams: { query, type, location, remote },
+}: PageProps) {
   const filterValues: JobFiltervalues = {
     query,
     type,
     location,
     remote: remote === "true",
-  }
+  };
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
-        <H1>
-          Developper Jobs
-        </H1>
+        <H1>{getTitle(filterValues)}</H1>
         <p className="text-muted-foreground"> Find your dream job.</p>
       </div>
       <section className="flex flex-col gap-4 md:flex-row">
-        <JobFilterSidebar defaultValues={filterValues}/>
-        <JobResults filterValues={filterValues}/>
+        <JobFilterSidebar defaultValues={filterValues} />
+        <JobResults filterValues={filterValues} />
       </section>
     </main>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/assets/logo.png"
+import {Button} from "./ui/button";
+
 export default function Navbar () {
     return <header className="shadow-sm">
-        <nav className="max-w-6xl m-auto">
-            Element 1
-            Element 2
+        <nav className="max-w-5xl m-auto px-3 py-5 flex items-center justify-between">
+            <Link href="/" className="flex items-center gap-3">
+                <Image
+                src={logo}
+                width={40}
+                height={40}
+                alt="Flow Jobs Logo"
+                />
+                <span className="text-xl font-bold tracking-tight"> Flow Jobs</span>
+            </Link>
+            <Button asChild>
+                <Link href="/jobs/new"> Post a Job</Link>
+            </Button>
         </nav>
     </header>
 }
