cb1b684c3162ba5ad572ddd0baa5ca469da3e8ad
Phfyx <97990186+Phfyx@users.noreply.github.com>
2024-03-07 17:01:54 +0100

    Delete admin/actions.ts and add isAdmin function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { formatDistanceToNowStrict } from "date-fns";
+import {UserResource} from "@clerk/types"
+import {User} from "@clerk/nextjs/server"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -22,4 +24,8 @@ export function toSlug(str: string) {
     .toLowerCase()
     .replace(/ /g, "-")
     .replace(/[^\w-]+/g, "");
+}
+
+export function isAdmin(user: UserResource | User){
+  return user.publicMetadata?.role === "admin";
 }
